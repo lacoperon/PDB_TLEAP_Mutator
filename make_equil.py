f5f790d6a0a095dadcbb6688ba7f0dae256a75a5
lacoperon <ejwilliams@wesleyan.edu>
2018-09-15 16:09:21 -0400

    Added correct HEAT and EQUIL .py generator files

---
+++ make_equil.py
@@ -0,0 +1,102 @@
+def generate_equil_sh(struct_name):
+
+    script_text = """
+#!/bin/bash
+#################### BSUB LINES TO CONFIGURE SCHEDULING OF JOB
+#BSUB -q amber128
+#BSUB -n 1
+#BSUB -R "rusage[gpu=1:mem=12288],span[hosts=1]"
+#BSUB -J GPU_{1}_GC_EQUIL
+#BSUB -o out
+#BSUB -e err
+
+## leave sufficient time between job submissions (30-60 secs)
+## the number of GPUs allocated matches -n value automatically
+## always reserve GPU (gpu=1), setting this to 0 is a cpu job only
+## reserve 6144 MB (5 GB + 20%) memory per GPU
+## run all processes (1<=n<=4)) on same node (hosts=1).
+
+
+# cuda 8 & mpich *NEW* for AMBER16
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export PATH=/usr/local/mpich-3.1.4/bin:/home/apps/amber/12cpu-only/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/mpich-3.1.4/lib:$LD_LIBRARY_PATH
+
+# unique job scratch dirs *NEW* for AMBER16
+MYSANSCRATCH=/sanscratch/
+MYLOCALSCRATCH=/localscratch/
+export MYSANSCRATCH MYLOCALSCRATCH
+#cd
+
+
+## AMBER we need to recreate env, /home/apps/amber/12cpu-only is already set
+export PATH=/share/apps/CENTOS6/python/2.7.9/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/CENTOS6/python/2.7.9/lib:$LD_LIBRARY_PATH
+source /usr/local/amber16/amber.sh
+
+# commands to run calculations go here.
+
+
+################ Start job ##########################
+# First clean out any diagnostic files from a previous run in this dir
+if [ -f temp.out ]; then
+     rm temp.out
+fi
+if [ -f time.log ]; then
+     rm time.log
+fi
+if [ -f err ]; then
+     rm err
+
+fi
+if [ -f out ]; then
+     rm out
+fi
+if [ -f mdinfo ]; then
+     rm mdinfo
+fi
+if [ -f logfile ]; then
+     rm logfile
+fi
+
+################################### MD EQUILIBRATION STEPS ###################################
+echo "MD EQUILIBRATION starting at " >> time.log
+date >> time.log
+
+############## Your Job Goes Here ############################
+#lava.mvapich2.wrapper # OLD for Amber12
+
+n78.mpich3.wrapper pmemd.cuda.MPI -O -i equil.in -p ../TLEAP/{0}_gc_wat.prmtop -c ../HEAT/{1}_gc_heat.rst -r {1}_gc_equil.rst -ref ../HEAT/{1}_gc_heat.rst -o 5JUP_GC_equil.out
+
+/share/apps/CENTOS6/amber/amber16/bin/ambpdb -p ../TLEAP/{0}_gc_wat.prmtop -c {1}_gc_equil.rst > {1}_gc_equil.pdb
+
+echo "equilibration equil_5JUP_GC is complete. View 5JUP_GC_equil.pdb with PyMol." | mailx -s "equil_{1}_GC" ejwilliams@wesleyan.edu
+""".format(struct_name, struct_name.upper())
+
+    f = open("EQUIL/run_equil.sh", "w")
+    f.write(script_text)
+    f.close()
+
+def generate_equil_in():
+    input_text = """
+100ps equilibration with 20kcal restraint
+ &cntrl
+  imin=0, nstlim=100000, dt=0.001,
+  irest=0, ntx=1,
+  ntc=2, ntf=2, ntb=2, ntp=1,
+  cut=8.0, iwrap=1, ig=-1,
+  ntpr=5000, ntwx=1000, ntwr=50000,
+  ntt=3, gamma_ln=1.0, temp0=300.0,
+  ntr=1, restraintmask='@CA,C,N,O', restraint_wt=20.0,
+  ioutfm=1,
+ /
+"""
+
+    f = open("EQUIL/equil.in", 'w')
+    f.write(input_text)
+    f.close()
+
+if __name__ == "__main__":
+    generate_equil_sh("5jup")
+    generate_equil_in()
---
+++ make_heat.py
@@ -0,0 +1,78 @@
+def generate_heat_sh(struct_name):
+
+    script_text = """
+    #!/bin/bash
+    #################### BSUB LINES TO CONFIGURE SCHEDULING OF JOB
+    #BSUB -q amber128
+    ####BSUB -q mwgpu
+    #BSUB -n 1
+    #BSUB -R "rusage[gpu=1:mem=12288],span[hosts=1]"
+    #BSUB -J GPU_{1}_HEAT
+    #BSUB -o out
+    #BSUB -e err
+
+    ## leave sufficient time between job submissions (30-60 secs)
+    ## the number of GPUs allocated matches -n value automatically
+    ## always reserve GPU (gpu=1), setting this to 0 is a cpu job only
+    ## reserve 6144 MB (5 GB + 20%) memory per GPU
+    ## run all processes (1<=n<=4)) on same node (hosts=1).
+
+
+    # cuda 8 & mpich *NEW* for AMBER16
+    export PATH=/usr/local/cuda/bin:$PATH
+    export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+    export PATH=/usr/local/mpich-3.1.4/bin:/home/apps/amber/12cpu-only/bin:$PATH
+    export LD_LIBRARY_PATH=/usr/local/mpich-3.1.4/lib:$LD_LIBRARY_PATH
+
+    # unique job scratch dirs *NEW* for AMBER16
+    MYSANSCRATCH=/sanscratch/
+    MYLOCALSCRATCH=/localscratch/
+    export MYSANSCRATCH MYLOCALSCRATCH
+
+    ## AMBER we need to recreate env, /home/apps/amber/12cpu-only is already set
+    export PATH=/share/apps/CENTOS6/python/2.7.9/bin:$PATH
+    export LD_LIBRARY_PATH=/share/apps/CENTOS6/python/2.7.9/lib:$LD_LIBRARY_PATH
+    source /usr/local/amber16/amber.sh
+
+    # commands to run calculations go here.
+
+    # MD sim: Heating
+    n78.mpich3.wrapper pmemd.cuda.MPI -O -i heat.in -o {0}_gc_heat.out -p ../TLEAP/{0}_gc_wat.prmtop -c ../EMIN/{1}_GC_min11.rst -r {1}_gc_heat.rst -ref ../EMIN/{1}_GC_min11.rst
+
+    # make pdb snapshot
+    /share/apps/CENTOS6/amber/amber16/bin/ambpdb -p ../TLEAP/{0}_gc_wat.prmtop -c {0}_gc_heat.rst > {0}_gc_heat.pdb
+
+    echo 'Your heating job heat_{1} is complete.' | mailx -s "heat_{1}" ejwilliams@wesleyan.edu
+    # Please make sure you're meaning to send me emails if you do
+""".format(struct_name, struct_name.upper())
+
+    f = open("HEAT/run_heat.sh", "w")
+    f.write(script_text)
+    f.close()
+
+def generate_heat_in():
+    input_text = """
+heating: MD with restraint on molecule
+ &cntrl
+  imin=0,
+  irest=0,
+  ntx=1,
+  ntb=1,
+  cut=10,
+  ntr=1, restraintmask=':1 - 495',restraint_wt=20.0,
+  ntc=2, ntf=2,
+  tempi=0.0,
+  temp0=300.0,
+  ntt=3, gamma_ln=1.0,
+  nstlim=10000, dt=0.002,
+  ntpr=1000, ntwx=1000, ntwr=10000
+ /
+"""
+
+    f = open("HEAT/heat.in", 'w')
+    f.write(input_text)
+    f.close()
+
+if __name__ == "__main__":
+    generate_heat_sh("5jup")
+    generate_heat_in()
